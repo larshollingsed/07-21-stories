f393cf8ee3e09e059c5b64230bc880117d2c800e
Lars Hollingsed <larshollingsed@gmail.com>
2015-07-21 12:37:37 -0500

    Added new user view and creation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,21 @@ class UsersController < ApplicationController
   def list
     @users = User.all
   end
+  
+  def new
+    @user = User.new
+  end
+  
+  def create
+    name = params["user"]["name"]
+    email = params["user"]["email"]
+    prepassword = params["user"]["password"]
+    password = BCrypt::Password.create(prepassword)
+    @user = User.create({"name" => name, "email" => email, "password" => password})
+    if @user.valid?
+      redirect "/users/login"
+    else
+      render :"/users/new"
+    end
+  end
 end
